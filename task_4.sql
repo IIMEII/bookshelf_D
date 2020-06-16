a54c6b81ea95b4b2ac2fb48f00d00ddbc5b05535
IIMEII <59262928+IIMEII@users.noreply.github.com>
2020-06-16 10:34:25 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -2,13 +2,17 @@
 SELECT * FROM студенты.students where Сourse = '1' And Name_of_specialty = 'менеджмент';
 
 #задание 3: Вывести всех девочек, учащихся на “3” курсе.
-SELECT * FROM студенты.students where Course = '3' And Gender = 'female';
+SELECT * FROM студенты.students where Сourse = '3' And Gender = 'female';
 
 #задание 4: Вывести количество студентов каждого курса, результат сгруппировать по специальности.
 SELECT count(Student_name), Name_of_specialty from студенты.students group by Name_of_specialty ;
 
 #задание 5: Вывести три самых популярных мужских имен среди студентов и посчитать сколько раз они встречаются.
 SELECT Student_name, count(Student_name) from студенты.students where Gender ='male' group by Student_name order by count(Student_name) desc limit 3;
+
+#задание 6:Посчитать сколько студентов проходят практику на каждом из курсов, результат сгруппировать по курсу.
+SELECT Сourse, count(Student_name) FROM студенты.students where  Discipline_name='Практика' group by(Сourse);
+"проверочный запрос  SELECT count(Discipline_name) FROM студенты.students where Discipline_name='Практика';   "
 
 #задание 7: Вывести троих самых старших студентов.
 SELECT Student_name, (year(current_date)-year(Date_of_Birth)) from студенты.students order by(Date_of_Birth) limit 3;
