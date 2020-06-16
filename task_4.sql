c8553f4bcfd8cdbb3b22e25cc0fcbfffeb9b2b30
IIMEII <59262928+IIMEII@users.noreply.github.com>
2020-06-16 11:37:49 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -15,8 +15,8 @@ SELECT Student_name, count(Student_name) from студенты.students where Ge
 SELECT Сourse, count(Student_name) FROM студенты.students where  Discipline_name='Практика' group by(Сourse);
 "проверочный запрос  SELECT count(Discipline_name) FROM студенты.students where Discipline_name='Практика';   "
 
-#задание 7: Вывести троих самых старших студентов.!!
+#задание 7: Вывести троих самых старших студентов.
 SELECT Student_name, (year(current_date)-year(Date_of_Birth)) from студенты.students order by(Date_of_Birth) limit 3;
 
-#задание 8: Посчитать средний возраст студентов, посещающих математику.!!
-SELECT Name_of_specialty, avg(Date_of_Birth) from студенты.students where Name_of_specialty ='Математика' ;
+#задание 8: Посчитать средний возраст студентов, посещающих математику.
+SSELECT avg (year(current_date)-(Date_of_Birth)) from студенты.students;
